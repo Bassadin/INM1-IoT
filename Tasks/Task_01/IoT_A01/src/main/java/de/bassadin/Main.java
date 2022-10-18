f58e9970a99c1113b2dc2f6860e4d759e81d31a7
Bastian Hodapp <bastianhodapp@gmail.com>
2022-10-18 16:56:06 +0200

    add example code

--- Tasks/Task_01/IoT_A01/src/main/java/de/bassadin/Main.java
+++ Tasks/Task_01/IoT_A01/src/main/java/de/bassadin/Main.java
@@ -1,7 +1,35 @@
 package de.bassadin;
 
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import java.security.Key;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        String data = "Satz, den man verschlüsseln kann, muss man aber nicht!";
+        try {
+            KeyGenerator keygen = KeyGenerator.getInstance("AES");
+            keygen.init(256);//,random);
+
+            Key secretKey = keygen.generateKey();
+            Cipher aesCipherInstance = Cipher.getInstance("AES");
+
+            byte[] dataAsBytes = data.getBytes("UTF8");
+            aesCipherInstance.init(Cipher.ENCRYPT_MODE, secretKey);
+
+            byte[] encryptedBytes = aesCipherInstance.doFinal(dataAsBytes);
+            String encryptedString = new String(encryptedBytes);
+            aesCipherInstance = Cipher.getInstance("AES");
+            aesCipherInstance.init(Cipher.DECRYPT_MODE, secretKey);
+
+            byte[] decryptedBytes = aesCipherInstance.doFinal(encryptedBytes);
+            String decryptedString = new String(decryptedBytes);
+
+            System.out.println("Original String : (" + data + ")");
+            System.out.println("Encrypt String : (" + encryptedString + ")");
+            System.out.println("Decrypt String : (" + decryptedString + ")");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
