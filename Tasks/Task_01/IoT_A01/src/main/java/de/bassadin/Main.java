ff061d409d0ee36dfc61a93ac486a77c276659e3
Bastian Hodapp <bastianhodapp@gmail.com>
2022-10-18 17:02:01 +0200

    add java mqtt client example code

--- Tasks/Task_01/IoT_A01/src/main/java/de/bassadin/Main.java
+++ Tasks/Task_01/IoT_A01/src/main/java/de/bassadin/Main.java
@@ -1,11 +1,49 @@
 package de.bassadin;
 
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import java.security.Key;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        // https://github.com/eclipse/paho.mqtt.java
+        String topic = "MQTT Examples";
+        String content = "Message from MqttPublishSample";
+        int qos = 2;
+        String brokerHostName = "tcp://221419df-1da2-441d-85a2-451d3809358b.fr.bw-cloud-instance.org";
+        String clientId = "JavaSample";
+        MemoryPersistence persistence = new MemoryPersistence();
+
+        try {
+            MqttClient sampleClient = new MqttClient(brokerHostName, clientId, persistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            System.out.println("Connecting to broker: " + brokerHostName);
+            sampleClient.connect(connOpts);
+            System.out.println("Connected");
+            System.out.println("Publishing message: " + content);
+            MqttMessage message = new MqttMessage(content.getBytes());
+            message.setQos(qos);
+            sampleClient.publish(topic, message);
+            System.out.println("Message published");
+            sampleClient.disconnect();
+            System.out.println("Disconnected");
+            System.exit(0);
+        } catch (MqttException me) {
+            System.out.println("reason " + me.getReasonCode());
+            System.out.println("msg " + me.getMessage());
+            System.out.println("loc " + me.getLocalizedMessage());
+            System.out.println("cause " + me.getCause());
+            System.out.println("excep " + me);
+            me.printStackTrace();
+        }
+
         String data = "Satz, den man verschlüsseln kann, muss man aber nicht!";
         try {
             KeyGenerator keygen = KeyGenerator.getInstance("AES");
