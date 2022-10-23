543fa0eb25dde44d6a47ea0a84e6cceafdf07878
Bastian Hodapp <bastianhodapp@gmail.com>
2022-10-23 12:59:49 +0200

    split code into two client classes

--- Tasks/Task_01/IoT_A01/src/main/java/de/bassadin/HellmanMQTTClient.java
+++ Tasks/Task_01/IoT_A01/src/main/java/de/bassadin/HellmanMQTTClient.java
@@ -7,7 +7,6 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 public class HellmanMQTTClient {
     static final int mqttQOS = 2;
     private String clientId = "Machine Alice";
-    private String topic = "MQTT Examples";
     static String brokerHostName = "tcp://221419df-1da2-441d-85a2-451d3809358b.fr.bw-cloud-instance.org";
 
     private MqttClient mqttClient;
@@ -25,21 +24,16 @@ public class HellmanMQTTClient {
         System.out.println("Disconnected");
     }
 
-    public HellmanMQTTClient(String topic) throws MqttException {
+    public HellmanMQTTClient(String clientId) throws MqttException {
         this();
-        this.topic = topic;
-    }
-
-    public HellmanMQTTClient(String topic, String clientId) throws MqttException {
-        this(topic);
         this.clientId = clientId;
     }
 
-    public void publishMqttMessage(String message) throws MqttException {
+    public void publishMqttMessage(String message, String messageTopic) throws MqttException {
         MqttMessage mqttMessage = new MqttMessage(message.getBytes());
         mqttMessage.setQos(mqttQOS);
 
-        mqttClient.publish(topic, mqttMessage);
+        mqttClient.publish(messageTopic, mqttMessage);
         System.out.println("Message published");
     }
 }
