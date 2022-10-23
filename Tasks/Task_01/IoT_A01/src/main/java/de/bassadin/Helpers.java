aef32ae0e0c990318d256392d199c0d4782ec8a0
Bastian Hodapp <bastianhodapp@gmail.com>
2022-10-23 13:02:32 +0200

    make claculate method public static

--- Tasks/Task_01/IoT_A01/src/main/java/de/bassadin/Helpers.java
+++ Tasks/Task_01/IoT_A01/src/main/java/de/bassadin/Helpers.java
@@ -4,7 +4,7 @@ public class Helpers {
     static final Long publicKey1 = 3461049572396L;
     static final Long publicKey2 = 98751942343464357L;
 
-    private Long calculateDiffieHellmanKey(Long publicKey1, Long privateKey, Long publicKey2) {
+    public static Long calculateDiffieHellmanKey(Long publicKey1, Long privateKey, Long publicKey2) {
         if (privateKey == 1) {
             return publicKey1;
         } else {
