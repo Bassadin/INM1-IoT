a2fb282d4b45db55d610317fad55adc731478bbd
Bastian Hodapp <bastianhodapp@gmail.com>
2022-11-02 15:43:49 +0100

    Auto stash before merge of "main" and "origin/main"

--- Tasks/Task_01/IoT_A01/src/main/java/de/bassadin/Helpers.java
+++ Tasks/Task_01/IoT_A01/src/main/java/de/bassadin/Helpers.java
@@ -1,5 +1,10 @@
 package de.bassadin;
 
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Arrays;
+import java.util.Base64;
 import java.util.Random;
 
 public class Helpers {
@@ -26,5 +31,36 @@ public class Helpers {
 
     public static double randomFloatBetween(double min, double max) {
         return min + Math.random() * (max - min);
+    }
+
+    public static String encryptMessageWithKey(final Long secretLong, final String data) {
+        byte[] decodedKey = Base64.getDecoder().decode(secretLong.toString());
+
+        try {
+            Cipher cipher = Cipher.getInstance("AES");
+            SecretKey originalKey = new SecretKeySpec(Arrays.copyOf(decodedKey, 16), "AES");
+            cipher.init(Cipher.ENCRYPT_MODE, originalKey);
+            byte[] cipherText = cipher.doFinal(data.getBytes("UTF-8"));
+            return Base64.getEncoder().encodeToString(cipherText);
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    "Error occured while encrypting data", e);
+        }
+
+    }
+
+    public static String decryptMessageWithKey(final Long secretLong, final String encryptedString) {
+        byte[] decodedKey = Base64.getDecoder().decode(secretLong.toString());
+
+        try {
+            Cipher cipher = Cipher.getInstance("AES");
+            SecretKey originalKey = new SecretKeySpec(Arrays.copyOf(decodedKey, 16), "AES");
+            cipher.init(Cipher.DECRYPT_MODE, originalKey);
+            byte[] cipherText = cipher.doFinal(Base64.getDecoder().decode(encryptedString));
+            return new String(cipherText);
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    "Error occured while decrypting data", e);
+        }
     }
 }
